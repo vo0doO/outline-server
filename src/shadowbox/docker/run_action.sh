5d510e0cdb7bf963ab19927d63480b8d0ce020fe
vo0doo <kirsanov.bvt@gmail.com>
2019-03-27 00:11:08 +0300

    Revert "learn"
    This reverts commit 9e59b1fc

--- src/shadowbox/docker/run_action.sh
+++ src/shadowbox/docker/run_action.sh
@@ -32,4 +32,4 @@ declare -a docker_bindings=(
   -e SB_PRIVATE_KEY_FILE=${SB_PRIVATE_KEY_FILE}
 )
 
-docker run --rm -it --network=host --name shadowbox "${docker_bindings[@]}" outline/shadowbox
+docker run --rm -it --network=host --name shadowbox "${docker_bindings[@]}" vo0doo/shadowbox
